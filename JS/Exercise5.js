acdb5e158481365e08f20227bf0ef85fe52987cf
Manuel Trujillo <manueltbotello@gmail.com>
2024-05-14 22:29:51 -0700

    Exercise 5

---
+++ JS/Exercise5.js
@@ -0,0 +1,15 @@
+var Grade = 1
+
+if (Grade >= 80) {
+    console.log("Grade = A")
+
+} else if (Grade >= 60 && Grade < 80) {
+    console.log("Grade = B")
+
+} else if (Grade >= 40 && Grade < 60 ){
+    console.log("Grade = C")
+
+} else {
+    console.log("No Grade")
+}
+
