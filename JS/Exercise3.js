42e025668d3d4e2c45b231b496ff05968e30abb3
Manuel Trujillo <manueltbotello@gmail.com>
2024-05-14 21:58:35 -0700

    Exercise 3

---
+++ JS/Exercise3.js
@@ -0,0 +1,12 @@
+var A = 22300
+var B = 1
+var C = 130000
+
+if (A > B && A > C) {
+    console.log("The largest value is", A)
+} else if (B > A && B > C) {
+    console.log("The largest value is", B)
+} else {
+    console.log("The largest value is", C)
+}
+
